51cebc7abeb4279e88a8fd6bccfcd93a2d05d8ca
Corrie Graham <corriegdev@gmail.com>
2025-02-13 11:32:18 +0200

    auth: basic auth added

--- src/routes/v1/auth.ts
+++ src/routes/v1/auth.ts
@@ -1,28 +1,43 @@
 import { Request, Response, Router } from 'express';
-import bcrypt from 'bcrypt';
+import { AuthService } from '../../services';
 
 const AuthRouter = Router();
-let password: string;
 
 AuthRouter.post('/register', async (request: Request, response: Response) => {
 	try {
-		password = await bcrypt.hash(request.body.password, 10);
-		response.json({
-			password,
-		});
+		const authorisedResponse = await AuthService.register(request.body);
+
+		const _t = authorisedResponse._t!;
+		delete authorisedResponse._t;
+
+		response
+			.status(201)
+			.setHeader('x-invoiced-token', _t)
+			.json(authorisedResponse);
 	} catch (e) {
-		console.log('Something went wrong');
+		console.error(e);
+		response.status(500).json({
+			message: e,
+		});
 	}
 });
 
 AuthRouter.post('/login', async (request: Request, response: Response) => {
 	try {
-		response.json({
-			status: await bcrypt.compare(request.body.password, password),
-		});
+		const authorisedResponse = await AuthService.login(request.body);
+
+		const _t = authorisedResponse._t!;
+		delete authorisedResponse._t;
+
+		response
+			.status(201)
+			.setHeader('x-invoiced-token', _t)
+			.json(authorisedResponse);
 	} catch (e) {
-		console.log('Something went wrong');
-		response.send('shitteroo');
+		console.log(e);
+		response.status(500).json({
+			message: e,
+		});
 	}
 });
 
